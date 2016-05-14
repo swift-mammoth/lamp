c30ce76ac65bea1bd9ba89f484b402a808f9f7a3
Z K <Z K>
2016-05-15 09:50:39 +1000

    test database connection from index.php

--- provision/puppet/modules/joker/manifests/config.pp
+++ provision/puppet/modules/joker/manifests/config.pp
@@ -1,7 +1,17 @@
-class joker::config {
+class joker::config (
+  $db_name, 
+  $db_user,    
+  $db_password,
+  $db_host,
+){
 
   file { '/var/www/html/phpinfo.php':
     ensure => file,
     content => file("${module_name}/phpinfo.php")
+  }
+
+  file { '/var/www/html/index.php':
+    ensure => file,
+    content => template("${module_name}/index.php.erb")
   }
 }
--- provision/puppet/modules/joker/manifests/init.pp
+++ provision/puppet/modules/joker/manifests/init.pp
@@ -1,6 +1,16 @@
-class joker {
+class joker (
+  $db_name     = 'jokes',
+  $db_user     = 'joker',
+  $db_password = 'userpassword',
+  $db_host     = '127.0.0.1', 
+){
 
-  class { 'joker::config': }
+  class { 'joker::config': 
+    db_name      => $db_name,
+    db_user      => $db_user,    
+    db_password  => $db_password,
+    db_host      => $db_host, 
+  }
 
   contain 'joker::config'
 }
